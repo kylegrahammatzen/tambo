0d727f7ad1041cbc66adf2ccb575ee4523ee316b
Kyle <kgrahammatzen@gmail.com>
2025-08-07 20:38:25 -0400

    feat(dark mode): add updated fumadocs layout

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <body className="flex flex-col min-h-screen">
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
-          enableSystem={false}
+          defaultTheme="system"
+          enableSystem={true}
         >
           <RootProvider>{children as React.ReactNode}</RootProvider>
         </ThemeProvider>
